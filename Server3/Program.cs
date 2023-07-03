209e8e1c034fc696eaf9aeee94e36824d8ad1cbd
majikojima <ykk61890@yahoo.co.jp>
2023-07-03 22:09:09 +0900

    Replace

--- Server3/Program.cs
+++ Server3/Program.cs
@@ -4,25 +4,14 @@ var app = builder.Build();
 app.MapGet("/", async ctx =>
 {
     ctx.Response.ContentType = "text/html; charset=utf-8";
-
+    // ランダム
     var r = new Random();
     var num = r.Next();
-
+    // 時刻
     var date = DateTime.Now;
-
-    await ctx.Response.WriteAsync($@"<!DOCTYPE html>
-    <html>
-    <head>
-        <title>ASP.NET Core</title>
-    </head>
-    <body>
-        <h1>ASP.NET Core</h1>
-        <p>random number: {num}</p>
-        <h2>date</h2>
-        <p>{date}</p>
-    </body>
-    </html>
-    ");
+    // テンプレート
+    var template = "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>インデックスページだよ</title></head><body><h1>ほげー</h1><h2>ランダム</h2><p>%num%</p><h2>時刻</h2><p>%date%</p></body></html>";
+    await ctx.Response.WriteAsync(template.Replace("%num%", num.ToString()).Replace("%date%", date.ToString()));
 });
 
 app.Run();
